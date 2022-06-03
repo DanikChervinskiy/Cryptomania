649f5f0e649b2c50a40380569649a4a4a3d3115e
DanikChervinskiy <93842618+DanikChervinskiy@users.noreply.github.com>
2022-06-03 14:27:30 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-// function userProgress(time){
-//     var start = 0;
-//     var time = Math.round(time*1000/100);
-//     var progressElement = document.getElementById('user-progres');
-//     var intervalId=setInterval(function(){
-//         if (start>100) {
-//             clearInterval(intervalId);
-//             userProgressCallBack();
-//         }
-// else {
-//     progressElement.value = start;
-//     }
-//     start++;
-//     }, time);
-// }
-
-const progress = document.querySelector('.progress-done');
-
-progress.style.width = progress.getAttribute('data-done') + '%';
-progress.style.opacity = 1;
-
-
-
-const time = 7000;
+const time = 6000;
 const step = 1;
 
 function outNum(num, elem) {
